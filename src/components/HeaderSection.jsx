1d5153f1d762dc25efcc93c1e591c66ba3ae5f72
andi aguirre <darckisgamer@gmail.com>
2024-11-30 12:24:13 -0500

    add total found cuntries at the header section

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,9 +1,13 @@
 import InputSearch from "../components/InputSearch";
+import { useContextCountry } from "../hooks/useContextCountry";
 const HeaderSection = () => {
+  const { content } = useContextCountry();
   return (
     <header>
       <div className="flex flex-col gap-2 mb-5 sm:flex-row sm:justify-between sm:items-center  ">
-        <p className="font-bold text-gray-design-two ">Found 234 countries</p>
+        <p className="font-bold text-gray-design-two ">
+          Found {content.length} countries
+        </p>
         <InputSearch />
       </div>
     </header>
